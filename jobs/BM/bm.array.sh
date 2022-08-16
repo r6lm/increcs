3f976752cd2e8fdc77da17b22b6b1787e7bc5c91
r6lm <rdgolaramolina@gmail.com>
2022-08-16 14:18:45 +0100

    wip: array jobs trial

--- jobs/BM/bm.array.sh
+++ jobs/BM/bm.array.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
 # Grid Engine options (lines prefixed with #$)
-#$ -cwd                 
-#$ -l h_vmem=2G
+#$ -N BM
+#$ -l h_rt=24:00:00
+#
+# Set working directory to the directory where the job is submitted from:
+#$ -cwd
+#
+# Request one GPU: 
+#$ -pe gpu-titanx 1
+#
+# Request system RAM 
+# the total system RAM available to the job is the value specified here multiplied by 
+# the number of requested GPUs (above)
+#$ -l h_vmem=50G
+# send emails
+#$ -M ro6lm@outlook.com
+#$ -m beas
  
 bash s$SGE_TASK_ID/s$SGE_TASK_ID.sh 
